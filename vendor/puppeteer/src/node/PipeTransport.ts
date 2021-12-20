9a9c4fb246788a543736ac5e839ec82e417751de
Ratson <github@ratson.name>
2021-12-20 20:08:11 +0800

    chore: update std to v0.118.0

--- vendor/puppeteer/src/node/PipeTransport.ts
+++ vendor/puppeteer/src/node/PipeTransport.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Buffer } from 'https://deno.land/std@0.116.0/node/buffer.ts';
+import { Buffer } from 'https://deno.land/std@0.118.0/node/buffer.ts';
 import {
   helper,
   debugError,
