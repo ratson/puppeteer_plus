0cc081be8b1a0b40034b4f97fe495a0ec836d07d
Ratson <github@ratson.name>
2021-12-23 01:33:02 +0800

    Squashed 'vendor/puppeteer/' changes from d0cb9e257..71cef32f6
    71cef32f6 chore(release): mark v13.0.1 (#7865)
    8d8e874b0 fix: make sure ElementHandle.waitForSelector is evaluated in the right context (#7843)
    1c44551f1 fix: predicate arguments for waitForFunction (#7845)
    36207c5ef fix: disable a test failing on Firefox (#7846)
    3e3a90be5 docs: fix typo (#7839)
    acdc67bd0 chore: bump version to v13.0.0-post (#7833)
    
    git-subtree-dir: vendor/puppeteer
    git-subtree-split: 71cef32f6d0420883baef87c1639dbc2def819a0

--- src/common/DOMWorld.ts
+++ src/common/DOMWorld.ts
@@ -645,6 +645,7 @@ export class DOMWorld {
     const waitTaskOptions: WaitTaskOptions = {
       domWorld: this,
       predicateBody: helper.makePredicateString(predicate, queryOne),
+      predicateAcceptsContextElement: true,
       title,
       polling,
       timeout,
@@ -691,6 +692,7 @@ export class DOMWorld {
     const waitTaskOptions: WaitTaskOptions = {
       domWorld: this,
       predicateBody: helper.makePredicateString(predicate),
+      predicateAcceptsContextElement: true,
       title,
       polling,
       timeout,
@@ -717,6 +719,7 @@ export class DOMWorld {
     const waitTaskOptions: WaitTaskOptions = {
       domWorld: this,
       predicateBody: pageFunction,
+      predicateAcceptsContextElement: false,
       title: 'function',
       polling,
       timeout,
@@ -737,6 +740,7 @@ export class DOMWorld {
 export interface WaitTaskOptions {
   domWorld: DOMWorld;
   predicateBody: Function | string;
+  predicateAcceptsContextElement: boolean;
   title: string;
   polling: string | number;
   timeout: number;
@@ -753,6 +757,7 @@ export class WaitTask {
   _polling: string | number;
   _timeout: number;
   _predicateBody: string;
+  _predicateAcceptsContextElement: boolean;
   _args: SerializableOrJSHandle[];
   _binding: PageBinding;
   _runCount = 0;
@@ -761,7 +766,7 @@ export class WaitTask {
   _reject: (x: Error) => void;
   _timeoutTimer?: NodeJS.Timeout;
   _terminated = false;
-  _root: ElementHandle;
+  _root: ElementHandle = null;
 
   constructor(options: WaitTaskOptions) {
     if (helper.isString(options.polling))
@@ -786,6 +791,8 @@ export class WaitTask {
     this._timeout = options.timeout;
     this._root = options.root;
     this._predicateBody = getPredicateBody(options.predicateBody);
+    this._predicateAcceptsContextElement =
+      options.predicateAcceptsContextElement;
     this._args = options.args;
     this._binding = options.binding;
     this._runCount = 0;
@@ -831,24 +838,15 @@ export class WaitTask {
     }
     if (this._terminated || runCount !== this._runCount) return;
     try {
-      if (this._root) {
-        success = await this._root.evaluateHandle(
-          waitForPredicatePageFunction,
-          this._predicateBody,
-          this._polling,
-          this._timeout,
-          ...this._args
-        );
-      } else {
-        success = await context.evaluateHandle(
-          waitForPredicatePageFunction,
-          null,
-          this._predicateBody,
-          this._polling,
-          this._timeout,
-          ...this._args
-        );
-      }
+      success = await context.evaluateHandle(
+        waitForPredicatePageFunction,
+        this._root || null,
+        this._predicateBody,
+        this._predicateAcceptsContextElement,
+        this._polling,
+        this._timeout,
+        ...this._args
+      );
     } catch (error_) {
       error = error_;
     }
@@ -911,6 +909,7 @@ export class WaitTask {
 async function waitForPredicatePageFunction(
   root: Element | Document | null,
   predicateBody: string,
+  predicateAcceptsContextElement: boolean,
   polling: string,
   timeout: number,
   ...args: unknown[]
@@ -927,7 +926,9 @@ async function waitForPredicatePageFunction(
    * @returns {!Promise<*>}
    */
   async function pollMutation(): Promise<unknown> {
-    const success = await predicate(root, ...args);
+    const success = predicateAcceptsContextElement
+      ? await predicate(root, ...args)
+      : await predicate(...args);
     if (success) return Promise.resolve(success);
 
     let fulfill;
@@ -937,7 +938,9 @@ async function waitForPredicatePageFunction(
         observer.disconnect();
         fulfill();
       }
-      const success = await predicate(root, ...args);
+      const success = predicateAcceptsContextElement
+        ? await predicate(root, ...args)
+        : await predicate(...args);
       if (success) {
         observer.disconnect();
         fulfill(success);
@@ -962,7 +965,9 @@ async function waitForPredicatePageFunction(
         fulfill();
         return;
       }
-      const success = await predicate(root, ...args);
+      const success = predicateAcceptsContextElement
+        ? await predicate(root, ...args)
+        : await predicate(...args);
       if (success) fulfill(success);
       else requestAnimationFrame(onRaf);
     }
@@ -979,7 +984,9 @@ async function waitForPredicatePageFunction(
         fulfill();
         return;
       }
-      const success = await predicate(root, ...args);
+      const success = predicateAcceptsContextElement
+        ? await predicate(root, ...args)
+        : await predicate(...args);
       if (success) fulfill(success);
       else setTimeout(onTimeout, pollInterval);
     }
