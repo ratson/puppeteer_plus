379da2c9b8ff1a3d1ce9568bd4d01fbc114884e2
Ratson <github@ratson.name>
2022-01-29 16:38:58 +0800

    test: use positional arguments

--- deps_dev.ts
+++ deps_dev.ts
@@ -7,7 +7,7 @@ export {
   assertStrictEquals,
 } from "https://deno.land/std@0.123.0/testing/asserts.ts";
 
-export * as subprocess from "https://deno.land/x/yxz@0.15.0/subprocess/mod.ts"
+export * as subprocess from "https://deno.land/x/yxz@0.15.0/subprocess/mod.ts";
 
 export function browserTest(
   name: string,
@@ -16,17 +16,13 @@ export function browserTest(
     & { launch?: PuppeteerNodeLaunchOptions }
     & Omit<Deno.TestDefinition, "name" | "fn"> = {},
 ) {
-  Deno.test({
-    name,
-    async fn() {
-      let browser: Browser | undefined = undefined;
-      try {
-        browser = await puppeteer.launch(launch);
-        await fn(browser);
-      } finally {
-        await browser?.close();
-      }
-    },
-    ...opts,
+  Deno.test(name, opts, async () => {
+    let browser: Browser | undefined = undefined;
+    try {
+      browser = await puppeteer.launch(launch);
+      await fn(browser);
+    } finally {
+      await browser?.close();
+    }
   });
 }
