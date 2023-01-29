b5fd993db421a11ecd0395d33afd6b17b0490612
Ratson <github@ratson.name>
2023-01-29 15:25:48 +0800

    feat: resuse upstream downloadBrowser()

--- mod.ts
+++ mod.ts
@@ -1,11 +1,10 @@
+import { downloadBrowser } from 'npm:puppeteer/internal/node/install.js';
 import { initializePuppeteerDeno } from "./src/initialize-deno.ts";
-import { downloadBrowser } from "./src/install.ts";
 
 export * from "./src/mod.ts";
 
 export const puppeteer = await initializePuppeteerDeno("puppeteer");
 export default puppeteer;
 
-if (!Deno.env.get("PUPPETEER_EXECUTABLE_PATH")) {
-  await downloadBrowser(puppeteer);
-}
+
+downloadBrowser();
