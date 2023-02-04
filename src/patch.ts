e91d899434423f91aabf884c7f057d3a1fa34fdd
Ratson <github@ratson.name>
2023-02-04 12:52:23 +0800

    fix: emulate file handle for PDF output

--- src/patch.ts
+++ src/patch.ts
@@ -1,5 +1,29 @@
 import { BrowserWebSocketTransport } from "npm:puppeteer-core/internal/common/BrowserWebSocketTransport.js";
 import { NodeWebSocketTransport } from "npm:puppeteer-core/internal/common/NodeWebSocketTransport.js";
+import { importFS } from "npm:puppeteer-core/internal/common/util.js";
 
 // @ts-expect-error patch
 NodeWebSocketTransport.create = BrowserWebSocketTransport.create;
+
+try {
+  const fs = await importFS();
+  const _open = fs.promises.open;
+  fs.promises.open = async (...args: Parameters<typeof _open>) => {
+    const f = await _open(...args);
+    if (typeof f === "number" && args.length === 2 && args[1] === "w+") {
+      return {
+        writeFile(s: string) {
+          return fs.promises.writeFile(f, s);
+        },
+        async close() {
+          return await new Promise((resolve) =>
+            fs.close(f, () => resolve(undefined))
+          );
+        },
+      };
+    }
+    return f;
+  };
+} catch {
+  // ignore error
+}
