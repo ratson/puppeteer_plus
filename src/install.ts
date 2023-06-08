f322df2654385693619a85f1ee9418ca169d8754
Ratson <github@ratson.name>
2023-06-08 20:23:44 +0800

    fix: avoid https error

--- src/install.ts
+++ src/install.ts
@@ -1,3 +1,12 @@
+import { getConfiguration } from "npm:puppeteer/internal/getConfiguration.js";
 import { downloadBrowser } from "npm:puppeteer/internal/node/install.js";
+
+const configuration = getConfiguration();
+if (typeof configuration.downloadBaseUrl === "undefined") {
+  Deno.env.set(
+    "PUPPETEER_DOWNLOAD_BASE_URL",
+    "http://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing",
+  );
+}
 
 await downloadBrowser();
